d7e6fc8d1ff9d676cd6109fdfae692bc5e3e890b
Ted <depietro.theodore@gmail.com>
2019-05-25 16:40:05 -0400

    asd

--- helpers/entities.js
+++ helpers/entities.js
@@ -1,17 +1,23 @@
-const Entity = require("./Entity.js");
-const { loadHeroSprite } = require("./sprites.js");
+const Entity = require('./Entity.js');
+const { loadHeroSprite } = require('./sprites.js');
 
 function createHero() {
   return loadHeroSprite().then(sprite => {
     const hero = new Entity();
 
     hero.draw = function drawHero(context) {
-      sprite.draw("idle", context, this.pos.x, this.pos.y);
+      sprite.draw('idle', context, this.pos.x, this.pos.y);
     };
 
     hero.update = function updateHero(deltaTime) {
-      this.pos.x += this.vel.x * deltaTime;
-      this.pos.y += this.vel.y * deltaTime;
+      if (this.pos.x < canvas.width - 200 && this.pos.x > 0) {
+        this.pos.x += this.vel.x * deltaTime;
+      }
+      if (this.pos.y < canvas.height - 200 && this.pos.y > 0) {
+        this.pos.y += this.vel.y * deltaTime;
+      }
+      //this.pos.x += this.vel.x * deltaTime;
+      // this.pos.y += this.vel.y * deltaTime;
     };
 
     return hero;
--- renderer.js
+++ renderer.js
@@ -1,38 +1,46 @@
-"use strict";
+'use strict';
 
 // DEPENDENCIES AND IMPORTS
-const Compositor = require("./helpers/Compositor");
-const Timer = require("./helpers/Timer");
-const { loadLevel } = require("./helpers/loaders.js");
-const { createHero } = require("./helpers/entities");
+const Compositor = require('./helpers/Compositor');
+const Timer = require('./helpers/Timer');
+const { loadLevel } = require('./helpers/loaders.js');
+const { createHero } = require('./helpers/entities');
 
-const { loadBackgroundSprites, loadStatic } = require("./helpers/sprites");
+const { loadBackgroundSprites, loadStatic } = require('./helpers/sprites');
+const Keyboard = require('./helpers/KeyboardState.js');
 
 const {
   createBackgroundLayer,
   createStaticLayer,
   createSpriteLayer
-} = require("./helpers/layers");
+} = require('./helpers/layers');
 
-const Entity = require("./helpers/Entity");
+const Entity = require('./helpers/Entity');
 
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+    y: ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height
+  };
+}
 // PROMISE ALL PERFORMS FOUR FUNCTIONS AND UPON SUCCESS THE DOT THEN HAPPENS WITH THOSE RESULTS
 
 Promise.all([
   createHero(),
   loadBackgroundSprites(),
   loadStatic(),
-  loadLevel("1-1")
+  loadLevel('1-1')
 ]).then(([hero, sprites, staticLayerSprite, level]) => {
   const comp = new Compositor();
 
   const gravity = 30;
 
   hero.pos.set(50, 900);
-  hero.vel.set(200, -2000);
+  hero.vel.set(200, -200);
 
   const staticLayer = createStaticLayer(staticLayerSprite);
   comp.layers.push(staticLayer);
@@ -51,4 +59,44 @@ Promise.all([
   };
 
   timer.start();
+
+  // input listeners
+  const input = new Keyboard();
+
+  input.addMapping(32, keyState => {
+    console.log(keyState);
+  });
+  input.listenTo(window);
+
+  window.addEventListener('mousedown', event => {
+    const click = getMousePos(canvas, event);
+    console.log(level.backgrounds);
+    hero.pos.set(click.x, click.y);
+    hero.vel.set(200, -200);
+
+    //context.moveTo(hero.pos[0], hero.pos[1]);
+    // context.moveTo(0, 0);
+    // context.lineTo(click.x, click.y);
+    // context.stroke();
+    // context.fillRect(click.x, click.y, 100, 100);
+    // const hitTerrain = false;
+    // obstacles.forEach(rect => {
+    //   if (
+    //     offsetX < rect.r &&
+    //     offsetX > rect.l &&
+    //     offsetY < rect.b &&
+    //     offsetY > rect.t
+    //   ) {
+    //     grapple = true;
+    //     desired.x = offsetX;
+    //     desired.y = offsetY;
+    //   }
+    // });
+  });
+
+  canvas.addEventListener('mouseup', ({ offsetX, offsetY }) => {
+    // grapple = false;
+    // followers[0].stopped = false;
+    // followers[0].stopping = false;
+  });
 });
