23fb54d1d0bb1c8cf344187965655cb2f2f76de2
Anthony Agnone <anthonyagnone@Anthonys-MBP.fios-router.home>
2019-05-24 22:32:17 -0400

    canvas renderer

--- index.js
+++ index.js
@@ -9,18 +9,10 @@ const { app, BrowserWindow } = electron;
 
 let mainWindow;
 
+// Listen for app to be ready
 app.on("ready", function() {
   // Create new window
-  mainWindow = new BrowserWindow({
-    width: 1920,
-    height: 1080,
-    frame: false,
-    center: true,
-    fullscreen: true,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+  mainWindow = new BrowserWindow({});
   // Load html in window
   mainWindow.loadURL(
     url.format({
