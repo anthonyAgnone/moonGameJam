f5dad3cc9bf1f91d932c96f55d484d67331cf182
Ted <depietro.theodore@gmail.com>
2019-05-26 00:22:27 -0400

    grappling logic

--- helpers/entities.js
+++ helpers/entities.js
@@ -9,14 +9,18 @@ function createHero() {
 
     hero.update = function updateHero(deltaTime) {
       if (this.pos.x < canvas.width - 200 && this.pos.x > 0) {
-        this.pos.x += this.vel.x * deltaTime;
-      } 
-      
-      if (this.pos.y < canvas.height - 200 && this.pos.y > 0) {
-        this.pos.y += this.vel.y * deltaTime;
+        this.pos.x += this.vel.x * deltaTime * 0.5;
       }
-      
+
+      if (this.pos.y < canvas.height - 200 && this.pos.y > 0) {
+        this.pos.y += this.vel.y * deltaTime * 0.5;
+      } else {
+        // this.vel.x = 0;
+        //this.vel.y = 0;
+      }
+
       this.lastPos.x = this.pos.x;
+      this.lastPos.y = this.pos.y;
       this.pos.x += this.vel.x * deltaTime;
       this.pos.y += this.vel.y * deltaTime;
       this.distance = this.vel.x * deltaTime;
@@ -24,30 +28,30 @@ function createHero() {
       currentFrame =
         Math.floor(Math.abs(this.pos.x - this.startPoint) / 21) % frames.length;
 
-      console.log(currentFrame);
+      // console.log(this.lastPos.x + ' ' + this.lastPos.y + ' ' + this.distance);
     };
 
     const frames = [
-      "run1",
-      "run2",
-      "run3",
-      "run4",
-      "run5",
-      "run6",
-      "run7",
-      "run8",
-      "run9",
-      "run10",
-      "run11",
-      "run12"
+      'run1',
+      'run2',
+      'run3',
+      'run4',
+      'run5',
+      'run6',
+      'run7',
+      'run8',
+      'run9',
+      'run10',
+      'run11',
+      'run12'
     ];
 
     function routeFrame(hero) {
       if (hero.lastPos.x !== hero.pos.x && hero.lastPos.y == hero.pos.y)
         return frames[currentFrame];
-      else if (hero.vel.y < 0) return "jump1";
-      else if (hero.vel.y > 0) return "fall1";
-      return "idle";
+      else if (hero.vel.y < 0) return 'jump1';
+      else if (hero.vel.y > 0) return 'fall1';
+      return 'idle';
     }
 
     hero.draw = function drawHero(context) {
